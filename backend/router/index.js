fdfe41c353ae88b2460a07362f88ad4c770c0aa7
DXelor <dzaliaspro@gmail.com>
2019-03-25 11:55:04 -0500

    Backend V2.0

--- backend/router/index.js
+++ backend/router/index.js
@@ -34,5 +34,60 @@ const routes = (app) => {
                 res.json({"error": true, "err": err});
             });
     });//--- ./filteroptions ---
+
+    //--- retorna los datos filtrados ---
+    app.get('/ciudad/:ciudadId/tipo/:tipoId/desde/:desdeVal/hasta/:hastaVal', (req, res)=>{
+        let params = req.params;
+        let datos = [];
+        storage.getDataAll()
+            .then(data => {
+                var ax = [];
+                var bx = [];
+                var datos = [];
+
+                ax = data.slice(); //--- retorna data en un array, como un nuevo objeto de array  ---
+
+                //--- validacion en la seleccion de las ciudades ---
+                if (params.ciudad != "todas"){
+                    //---  recorre y filtra cada una de las ciudades ---
+
+                    ax.forEach((key, i) => {
+                        if (key.Ciudad == params.ciudadId){ bx.push(key); }
+                    });
+                } else {
+                    bx = ax.slice();
+                }
+
+                //--- reiniciar arrays bx, ax y cada array retornado de este ---
+                ax= [];
+                ax = bx.slice();
+                bx = [];
+
+                //--- valida la seleccion de un tipo de propiedad ---
+
+                if (params.tipoId != "todos") {
+                    //--- valida y filtra segun el tipo de propiedad seleccionada ---
+                    ax.forEach((key, i) =>{
+                        if (key.Tipo == params.tipoId) { bx.push(key); }
+                    });
+                } else {
+                    bx = ax.slice();
+                }
+
+                //--- recorre y filtra si esta entre los valores seleccionados ---
+
+                bx.forEach((key, i)=>{
+                    let valor = parseInt(key.Precio.replace("$", "").replace(",", ""));
+                    if (valor >= parseInt(params.desdeVal) && valor <= parseInt(params.hastaVal)){
+                        datos.push(key);
+                    }
+                });
+
+                res.status(200).json({ datos, params });
+            })
+            .catch((err) =>{
+                res.json({ "error": true, "err": err });
+            });
+    });
 }
 module.exports = routes;
